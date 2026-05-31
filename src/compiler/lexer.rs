54d497df53b90ce1d85f56d18008dddd3e45a484
EduHoff <ehcs.business@gmail.com>
2026-05-31 14:37:15 -0300

    Agora os tokens possuí informação relacionado a linha e coluna

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -2,13 +2,43 @@ use std::{iter::Peekable, str::Chars};
 
 use crate::compiler::token::{Token, TokenType};
 
-fn read_identifier_or_keyword(chars: &mut Peekable<Chars>) -> Token {
+fn advance(chars: &mut Peekable<Chars>, line: &mut usize, column: &mut usize) {
+    let c = chars.peek();
+
+    if c == Some(&'\n') {
+        *line += 1;
+        *column = 1;
+        chars.next();
+    } else {
+        *column += 1;
+        chars.next();
+    }
+}
+
+fn skip_whitespace(chars: &mut Peekable<Chars>, line: &mut usize, column: &mut usize) {
+    while let Some(&c) = chars.peek() {
+        if c.is_whitespace() {
+            advance(chars, line, column);
+        } else {
+            break;
+        }
+    }
+}
+
+fn read_identifier_or_keyword(
+    chars: &mut Peekable<Chars>,
+    line: &mut usize,
+    column: &mut usize,
+) -> Token {
     let mut buf = String::new();
+
+    let current_line = *line;
+    let current_column = *column;
 
     while let Some(&c) = chars.peek() {
         if c.is_alphanumeric() || c == '_' {
             buf.push(c);
-            chars.next();
+            advance(chars, line, column);
         } else {
             break;
         }
@@ -30,7 +60,7 @@ fn read_identifier_or_keyword(chars: &mut Peekable<Chars>) -> Token {
         "async" => TokenType::Async,
         "void" => TokenType::Void,
         "int" => TokenType::Int,
-        "float" => TokenType::Float,
+        "double" => TokenType::Double,
         "char" => TokenType::Char,
         "bool" => TokenType::Bool,
         "true" => TokenType::True,
@@ -38,40 +68,52 @@ fn read_identifier_or_keyword(chars: &mut Peekable<Chars>) -> Token {
         _ => TokenType::Ident,
     };
 
-    Token::new(Some(buf), token_type)
+    Token::new(Some(buf), token_type, current_line, current_column)
 }
 
-fn read_number_literal(chars: &mut Peekable<Chars>) -> Token {
+fn read_number_literal(chars: &mut Peekable<Chars>, line: &mut usize, column: &mut usize) -> Token {
     let mut buf = String::new();
+
+    let current_line = *line;
+    let current_column = *column;
 
     while let Some(&c) = chars.peek() {
         if c.is_ascii_digit() || c == '.' {
             buf.push(c);
-            chars.next();
+            advance(chars, line, column);
         } else {
             break;
         }
     }
 
-    Token::new(Some(buf), TokenType::IntLit)
+    let token_type = if buf.contains('.') {
+        TokenType::DoubleLit
+    } else {
+        TokenType::IntLit
+    };
+
+    Token::new(Some(buf), token_type, current_line, current_column)
 }
 
 pub fn tokenize(str: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
 
+    let mut line: usize = 1;
+    let mut column: usize = 1;
+
     let mut chars = str.chars().peekable();
 
     while let Some(&c) = chars.peek() {
         if c.is_whitespace() {
-            chars.next();
+            skip_whitespace(&mut chars, &mut line, &mut column);
         } else if c.is_alphabetic() || c == '_' {
-            let token = read_identifier_or_keyword(&mut chars);
+            let token = read_identifier_or_keyword(&mut chars, &mut line, &mut column);
             tokens.push(token);
         } else if c.is_numeric() {
-            let token = read_number_literal(&mut chars);
+            let token = read_number_literal(&mut chars, &mut line, &mut column);
             tokens.push(token);
         } else {
-            chars.next();
+            advance(&mut chars, &mut line, &mut column);
         }
     }
 
--- src/compiler/token.rs
+++ src/compiler/token.rs
@@ -20,7 +20,7 @@ pub enum TokenType {
     // --- Built-in Data Types ---
     Void,
     Int,
-    Float,
+    Double,
     Char,
     Bool,
 
@@ -68,10 +68,10 @@ pub enum TokenType {
     CloseBracket, // ]
 
     // --- Dynamic Literals and Identifiers ---
-    IntLit,   // e.g., 42
-    FloatLit, // e.g., 3.14
-    CharLit,  // e.g., 'a'
-    Ident,    // Variable or function names, e.g., "my_variable"
+    IntLit,    // e.g., 42
+    DoubleLit, // e.g., 3.14
+    CharLit,   // e.g., 'a'
+    Ident,     // Variable or function names, e.g., "my_variable"
 }
 
 #[derive(Debug, Clone)]
@@ -83,12 +83,28 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn new(value: Option<String>, token_type: TokenType) -> Self {
+    pub fn new(value: Option<String>, token_type: TokenType, line: usize, column: usize) -> Self {
         Token {
             token_type,
             value,
-            line: 0,
-            column: 0,
+            line,
+            column,
         }
+    }
+
+    pub fn token_type(&self) -> &TokenType {
+        &self.token_type
+    }
+
+    pub fn value(&self) -> Option<&String> {
+        self.value.as_ref()
+    }
+
+    pub fn line(&self) -> &usize {
+        &self.line
+    }
+
+    pub fn column(&self) -> &usize {
+        &self.column
     }
 }
